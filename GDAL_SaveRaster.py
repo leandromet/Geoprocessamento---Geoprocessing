b6aedc1589c754bb867381e309aba5ae19f7bb1a
leandromet <leandro.biondo@florestal.gov.br>
2014-04-09 11:11:19 -0300

    Create GDAL_SaveRaster.py

---
+++ GDAL_SaveRaster.py
@@ -0,0 +1,36 @@
+
+from osgeo import gdal
+
+
+
+def save_raster ( output_name, raster_data, dataset, driver="GTiff" ):
+    """
+    A function to save a 1-band raster using GDAL to the file indicated
+    by ``output_name``. It requires a GDAL-accesible dataset to collect
+    the projection and geotransform.
+    """
+
+    # Open the reference dataset
+    g_input = gdal.Open ( dataset )
+    # Get the Geotransform vector
+    geo_transform = g_input.GetGeoTransform ()
+    x_size = g_input.RasterXSize # Raster xsize
+    y_size = g_input.RasterYSize # Raster ysize
+    srs = g_input.GetProjectionRef () # Projection
+    # Need a driver object. By default, we use GeoTIFF
+    if driver == "GTiff":
+        driver = gdal.GetDriverByName ( driver )
+        dataset_out = driver.Create ( output_name, x_size, y_size, 1, \
+                gdal.GDT_Float32, ['TFW=YES', \
+                'COMPRESS=LZW', 'TILED=YES'] )
+    else:
+        driver = gdal.GetDriverByName ( driver )
+        dataset_out = driver.Create ( output_name, x_size, y_size, 1, \
+                gdal.GDT_Float32 )
+
+    dataset_out.SetGeoTransform ( geo_transform )
+    dataset_out.SetProjection ( srs )
+    dataset_out.GetRasterBand ( 1 ).WriteArray ( \
+            raster_data.astype(np.float32) )
+    dataset_out.GetRasterBand ( 1 ).SetNoDataValue ( float(-999) )
+    dataset_out = None
