89272c7db5950b8f2113a2d54de4834c829c5384
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2014-05-19 16:41:07 -0300

    Update SQL_check_geo_consistency.sql
    12 functions

--- SQL_check_geo_consistency.sql
+++ SQL_check_geo_consistency.sql
@@ -233,3 +233,256 @@ SELECT * FROM scc.scc_val_parcelas(1);
 
 
 
+
+--FUNCAO PARA VALIDACAO E INCLUSAO DE PATIOS PRINCIPAIS NA UMF RETORNA "0" SE VALIDADO E SÃO COPIADOS OS REGISTROS PARA TABELA scc.scc_patio_PRINCIPAL, RETORNA NÚMERO DE PÁTIOS FORA DA UMF SE NÃO VÁLIDO. SE VÁLIDO E HOUVER PÁTIOS REPETIDOS, RETONA O TOTAL.
+
+CREATE OR REPLACE FUNCTION scc.scc_val_patio_principal(id_umf integer, OUT fora_umf numeric, OUT repetidos numeric) AS $$
+
+DECLARE org scc.temp_patio_principal%rowtype;
+DECLARE geom_umf geometry;
+BEGIN
+	geom_umf := ST_Union(ARRAY(SELECT shape FROM sde.umfs WHERE sde.umfs.id_umf = $1));
+	fora_umf := (SELECT count(temp_patio_principal.gid) FROM temp_patio_principal
+WHERE NOT ST_Intersects(geom_umf, temp_patio_principal.shape) AND ST_Distance(geom_umf, temp_patio_principal.shape) > 0);
+	IF fora_umf = 0
+		THEN
+				repetidos:=(SELECT count(scc.scc_patio_principal.objectid) FROM scc.temp_patio_principal, scc.scc_patio_principal
+				    WHERE temp_patio_principal.num_upa=scc_patio_principal.num_upa AND temp_patio_principal.area_ha=scc_patio_principal.area_ha
+				);
+
+				INSERT INTO scc.scc_patio_principal(num_upa, area_ha, shape) 
+	              		(SELECT scc.temp_patio_principal.num_upa, scc.temp_patio_principal.area_ha, scc.temp_patio_principal.shape
+				 FROM temp_patio_principal
+				  WHERE NOT EXISTS 
+				  (SELECT scc.scc_patio_principal.num_upa, scc.scc_patio_principal.area_ha FROM scc.temp_patio_principal, scc.scc_patio_principal
+				    WHERE temp_patio_principal.num_upa=scc_patio_principal.num_upa AND temp_patio_principal.area_ha=scc_patio_principal.area_ha
+				  )
+				);
+
+	END IF;
+
+END;
+$$LANGUAGE plpgsql;
+
+
+--SELECT * FROM scc.scc_val_patio_principal(1);
+
+
+
+
+
+--FUNCAO PARA VALIDACAO E INCLUSAO DE NASCENTES NA UMF RETORNA "0" SE VALIDADO E SÃO COPIADOS OS REGISTROS PARA TABELA scc.scc_nascentes, RETORNA NÚMERO DE NASCENTES FORA DA UMF SE NÃO VÁLIDO. SE VÁLIDO E HOUVER PÁTIOS REPETIDOS, RETONA O TOTAL.
+
+CREATE OR REPLACE FUNCTION scc.scc_val_nascentes(id_umf integer, OUT fora_umf numeric, OUT repetidos numeric) AS $$
+
+DECLARE org scc.temp_nascentes%rowtype;
+DECLARE geom_umf geometry;
+BEGIN
+	geom_umf := ST_Union(ARRAY(SELECT shape FROM sde.umfs WHERE sde.umfs.id_umf = $1));
+	fora_umf := (SELECT count(temp_nascentes.gid) FROM temp_nascentes
+WHERE NOT ST_Intersects(geom_umf, temp_nascentes.shape) AND ST_Distance(geom_umf, temp_nascentes.shape) > 0);
+	IF fora_umf = 0
+		THEN
+				repetidos:=(SELECT count(scc.scc_nascentes.objectid) FROM scc.temp_nascentes, scc.scc_nascentes
+				    WHERE temp_nascentes.num_upa=scc_nascentes.num_upa AND temp_nascentes.shape=scc_nascentes.shape
+				);
+
+				INSERT INTO scc.scc_nascentes(num_upa, area_ha, shape) 
+	              		(SELECT scc.temp_nascentes.num_upa, scc.temp_nascentes.shape
+				 FROM temp_nascentes
+				  WHERE NOT EXISTS 
+				  (SELECT scc.scc_nascentes.num_upa, scc.scc_nascentes.shape FROM scc.temp_nascentes, scc.scc_nascentes
+				    WHERE temp_nascentes.num_upa=scc_nascentes.num_upa AND temp_nascentes.shape=scc_nascentes.shape
+				  )
+				);
+
+	END IF;
+
+END;
+$$LANGUAGE plpgsql;
+
+
+--SELECT * FROM scc.scc_val_nascentes(1);
+
+
+
+
+
+--FUNCAO PARA VALIDACAO E INCLUSAO DE TRILHAS DE ARRASTE NA UMF RETORNA "0" SE VALIDADO E SÃO COPIADOS OS REGISTROS PARA TABELA scc.scc_trilhas_arraste, RETORNA NÚMERO DE TRILHAS FORA DA UMF SE NÃO VÁLIDO. SE VÁLIDO E HOUVER TRILHAS REPETIDAS, RETONA O TOTAL.
+
+CREATE OR REPLACE FUNCTION scc.scc_val_trilhas_arraste(id_umf integer, OUT est_val numeric, OUT est_rep numeric) AS $$
+
+DECLARE org scc.temp_trilhas_arraste%rowtype;
+DECLARE geom_umf geometry;
+
+BEGIN
+		geom_umf := ST_Union(ARRAY(SELECT shape FROM sde.umfs WHERE sde.umfs.id_umf = $1));
+	est_rep := 0;
+	est_val := (SELECT count(temp_trilhas_arraste.gid) FROM temp_trilhas_arraste WHERE NOT ST_Intersects(geom_umf, temp_trilhas_arraste.shape) AND ST_Distance(geom_umf, temp_trilhas_arraste.shape) > 0);
+	IF est_val = 0
+		THEN
+				est_val = 1;
+				est_rep := est_rep+(SELECT count(temp_trilhas_arraste.gid) FROM scc.scc_trilhas_arraste, scc.temp_trilhas_arraste 
+				  WHERE scc.scc_trilhas_arraste.comp_m=scc.temp_trilhas_arraste.comp_m
+					AND scc.scc_trilhas_arraste.shape=scc.temp_trilhas_arraste.shape);
+				INSERT INTO scc.scc_trilhas_arraste(num_upa, comp_m, shape) 
+	              		(SELECT temp_trilhas_arraste.num_upa, temp_trilhas_arraste.comp_m, temp_trilhas_arraste.shape
+				  FROM temp_trilhas_arraste
+				  WHERE NOT EXISTS (SELECT scc.scc_trilhas_arraste.num_upa, scc.scc_trilhas_arraste.comp_m, scc.scc_trilhas_arraste.shape FROM scc.scc_trilhas_arraste, scc.temp_trilhas_arraste 
+				  WHERE scc.scc_trilhas_arraste.comp_m=scc.temp_trilhas_arraste.comp_m
+					AND scc.scc_trilhas_arraste.shape=scc.temp_trilhas_arraste.shape)
+				);
+				
+	END IF;
+END;
+$$LANGUAGE plpgsql;
+
+
+SELECT * FROM scc.scc_val_trilhas_arraste(1);
+
+
+
+
+
+--FUNCAO PARA VALIDACAO E INCLUSAO DE APP NA UMF RETORNA "0" SE VALIDADO E SÃO COPIADOS OS REGISTROS PARA TABELA scc.scc_app, RETORNA NÚMERO APP FORA DA UMF SE NÃO VÁLIDO. SE VÁLIDO E HOUVER APP REPETIDAS, RETONA O TOTAL.
+
+CREATE OR REPLACE FUNCTION scc.scc_val_app(id_umf integer, OUT fora_umf numeric, OUT repetidos numeric) AS $$
+
+DECLARE org scc.temp_app%rowtype;
+DECLARE geom_umf geometry;
+BEGIN
+	geom_umf := ST_Union(ARRAY(SELECT shape FROM sde.umfs WHERE sde.umfs.id_umf = $1));
+	fora_umf := (SELECT count(temp_app.gid) FROM temp_app
+WHERE NOT ST_Intersects(geom_umf, temp_app.shape) AND ST_Distance(geom_umf, temp_app.shape) > 0);
+	IF fora_umf = 0
+		THEN
+				repetidos:=(SELECT count(scc.scc_app.objectid) FROM scc.temp_app, scc.scc_app
+				    WHERE temp_app.num_upa=scc_app.num_upa AND temp_app.area_ha=scc_app.area_ha
+				);
+
+				INSERT INTO scc.scc_app(num_upa, area_ha, shape) 
+	              		(SELECT scc.temp_app.num_upa, scc.temp_app.area_ha, scc.temp_app.shape
+				 FROM temp_app
+				  WHERE NOT EXISTS 
+				  (SELECT scc.scc_app.num_upa, scc.scc_app.area_ha FROM scc.temp_app, scc.scc_app
+				    WHERE temp_app.num_upa=scc_app.num_upa AND temp_app.area_ha=scc_app.area_ha
+				  )
+				);
+
+	END IF;
+
+END;
+$$LANGUAGE plpgsql;
+
+
+--SELECT * FROM scc.scc_val_app(1);
+
+
+
+
+
+
+--FUNCAO PARA VALIDACAO E INCLUSAO DE ESPELHOS D'ÁGUA NA UMF RETORNA "0" SE VALIDADO E SÃO COPIADOS OS REGISTROS PARA TABELA scc.scc_espelho_dagua, RETORNA NÚMERO DE ESPELHOS FORA DA UMF SE NÃO VÁLIDO. SE VÁLIDO E HOUVER ESPELHOS REPETIDOS, RETONA O TOTAL.
+
+CREATE OR REPLACE FUNCTION scc.scc_val_espelho_dagua(id_umf integer, OUT fora_umf numeric, OUT repetidos numeric) AS $$
+
+DECLARE org scc.temp_espelho_dagua%rowtype;
+DECLARE geom_umf geometry;
+BEGIN
+	geom_umf := ST_Union(ARRAY(SELECT shape FROM sde.umfs WHERE sde.umfs.id_umf = $1));
+	fora_umf := (SELECT count(temp_espelho_dagua.gid) FROM temp_espelho_dagua
+WHERE NOT ST_Intersects(geom_umf, temp_espelho_dagua.shape) AND ST_Distance(geom_umf, temp_espelho_dagua.shape) > 0);
+	IF fora_umf = 0
+		THEN
+				repetidos:=(SELECT count(scc.scc_espelho_dagua.objectid) FROM scc.temp_espelho_dagua, scc.scc_espelho_dagua
+				    WHERE temp_espelho_dagua.num_upa=scc_espelho_dagua.num_upa AND temp_espelho_dagua.area_ha=scc_espelho_dagua.area_ha
+				);
+
+				INSERT INTO scc.scc_espelho_dagua(num_upa, area_ha, shape) 
+	              		(SELECT scc.temp_espelho_dagua.num_upa, scc.temp_espelho_dagua.area_ha, scc.temp_espelho_dagua.shape
+				 FROM temp_espelho_dagua
+				  WHERE NOT EXISTS 
+				  (SELECT scc.scc_espelho_dagua.num_upa, scc.scc_espelho_dagua.area_ha FROM scc.temp_espelho_dagua, scc.scc_espelho_dagua
+				    WHERE temp_espelho_dagua.num_upa=scc_espelho_dagua.num_upa AND temp_espelho_dagua.area_ha=scc_espelho_dagua.area_ha
+				  )
+				);
+
+	END IF;
+
+END;
+$$LANGUAGE plpgsql;
+
+
+--SELECT * FROM scc.scc_val_espelho_dagua(1);
+
+
+
+
+--FUNCAO PARA VALIDACAO E INCLUSAO DE UNIDADES DE TRABALHO NA UMF RETORNA "0" SE VALIDADO E SÃO COPIADOS OS REGISTROS PARA TABELA scc.scc_unidade_trabalho, RETORNA NÚMERO DE UTS FORA DA UMF SE NÃO VÁLIDO. SE VÁLIDO E HOUVER UTS REPETIDOS, RETONA O TOTAL.
+
+CREATE OR REPLACE FUNCTION scc.scc_val_unidade_trabalho(id_umf integer, OUT fora_umf numeric, OUT repetidos numeric) AS $$
+
+DECLARE org scc.temp_unidade_trabalho%rowtype;
+DECLARE geom_umf geometry;
+BEGIN
+	geom_umf := ST_Union(ARRAY(SELECT shape FROM sde.umfs WHERE sde.umfs.id_umf = $1));
+	fora_umf := (SELECT count(temp_unidade_trabalho.gid) FROM temp_unidade_trabalho
+WHERE NOT ST_Intersects(geom_umf, temp_unidade_trabalho.shape) AND ST_Distance(geom_umf, temp_unidade_trabalho.shape) > 0);
+	IF fora_umf = 0
+		THEN
+				repetidos:=(SELECT count(scc.scc_unidade_trabalho.objectid) FROM scc.temp_unidade_trabalho, scc.scc_unidade_trabalho
+				    WHERE temp_unidade_trabalho.num_upa=scc_unidade_trabalho.num_upa AND temp_unidade_trabalho.area_ha=scc_unidade_trabalho.area_ha
+				);
+
+				INSERT INTO scc.scc_unidade_trabalho(num_upa, area_ha, cod_ut, shape) 
+	              		(SELECT scc.temp_unidade_trabalho.num_upa, scc.temp_unidade_trabalho.area_ha, scc.temp_unidade_trabalho.cod_ut, scc.temp_unidade_trabalho.shape
+				 FROM temp_unidade_trabalho
+				  WHERE NOT EXISTS 
+				  (SELECT scc.scc_unidade_trabalho.num_upa, scc.scc_unidade_trabalho.cod_ut FROM scc.temp_unidade_trabalho, scc.scc_unidade_trabalho
+				    WHERE temp_unidade_trabalho.num_upa=scc_unidade_trabalho.num_upa AND temp_unidade_trabalho.cod_ut=scc_unidade_trabalho.cod_ut
+				  )
+				);
+
+	END IF;
+
+END;
+$$LANGUAGE plpgsql;
+
+
+--SELECT * FROM scc.scc_val_unidade_trabalho(1);
+
+
+
+
+
+--FUNCAO PARA VALIDACAO E INCLUSAO DE HIDROGRAFIA NA UMF RETORNA "0" SE VALIDADO E SÃO COPIADOS OS REGISTROS PARA TABELA scc.scc_hidrografia, RETORNA NÚMERO DE HIDROGRAFIA FORA DA UMF SE NÃO VÁLIDO. SE VÁLIDO E HOUVER HIDROGRAFIA REPETIDAS, RETONA O TOTAL.
+
+CREATE OR REPLACE FUNCTION scc.scc_val_hidrografia(id_umf integer, OUT est_val numeric, OUT est_rep numeric) AS $$
+
+DECLARE org scc.temp_hidrografia%rowtype;
+DECLARE geom_umf geometry;
+
+BEGIN
+		geom_umf := ST_Union(ARRAY(SELECT shape FROM sde.umfs WHERE sde.umfs.id_umf = $1));
+	est_rep := 0;
+	est_val := (SELECT count(temp_hidrografia.gid) FROM temp_hidrografia WHERE NOT ST_Intersects(geom_umf, temp_hidrografia.shape) AND ST_Distance(geom_umf, temp_hidrografia.shape) > 0);
+	IF est_val = 0
+		THEN
+				est_val = 1;
+				est_rep := est_rep+(SELECT count(temp_hidrografia.gid) FROM scc.scc_hidrografia, scc.temp_hidrografia 
+				  WHERE scc.scc_hidrografia.comp_m=scc.temp_hidrografia.comp_m
+					AND scc.scc_hidrografia.shape=scc.temp_hidrografia.shape);
+				INSERT INTO scc.scc_hidrografia(num_upa, comp_m, shape) 
+	              		(SELECT temp_hidrografia.num_upa, temp_hidrografia.comp_m, temp_hidrografia.shape
+				  FROM temp_hidrografia
+				  WHERE NOT EXISTS (SELECT scc.scc_hidrografia.num_upa, scc.scc_hidrografia.comp_m, scc.scc_hidrografia.shape FROM scc.scc_hidrografia, scc.temp_hidrografia 
+				  WHERE scc.scc_hidrografia.comp_m=scc.temp_hidrografia.comp_m
+					AND scc.scc_hidrografia.shape=scc.temp_hidrografia.shape)
+				);
+				
+	END IF;
+END;
+$$LANGUAGE plpgsql;
+
+
+--SELECT * FROM scc.scc_val_hidrografia(1);
