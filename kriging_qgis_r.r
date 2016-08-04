df480fb1aca78a0ed208a7b153fdcf2f3fc3cd44
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-08-04 15:56:54 -0300

    Update kriging_qgis_r.r

--- kriging_qgis_r.r
+++ kriging_qgis_r.r
@@ -4,7 +4,7 @@
 ##Field=Field Layer
 ## by= number 0.1
 ##Output=output raster
-Bibliotecas utilizadas para a krigagem
+##Bibliotecas utilizadas para a krigagem
 library(gstat)
 library(rgl)
 library("spatstat")
@@ -13,14 +13,15 @@ install.packages("pls")
 library (pls)
 library(automap)
 library(raster)
-Abertura de dados e alocação de variáveis
+##Abertura de dados e alocação de variáveis
 Y<-as.factor(Layer[[Field]])
 attribut<-as.data.frame(Y)
 A<-as.numeric(Y)
 for(j in (1:length(levels(Y))))
 for(i in 1:dim(attribut)[1]){
 if (attribut[i,1]==levels(Y)[j]){
-A[i]=j } }	Carregamento das informações espaciais
+A[i]=j } }
+##Carregamento das informações espaciais
 coords<-coordinates(Layer)
 Mesure<- data.frame(LON=coords[,1], LAT=coords[,2], A)
 coordinates(Mesure)<-c("LON","LAT")
@@ -33,15 +34,15 @@ Seqy<-seq(MinY, MaxY, by=by)
 MSeqx<-rep(Seqx, length(Seqy))
 MSeqy<-rep(Seqy, length(Seqx))
 MSeqy <- sort(MSeqy, decreasing=F)
-Criação da planilha para execução no R
+##Criação da planilha para execução no R
 Grille <- data.frame(X=MSeqx, Y=MSeqy)
 coordinates(Grille)=c("X","Y")
 gridded(Grille)<-TRUE
-Extração do variograma para modelagem com modelo de crescimento exponencial
+##Extração do variograma com regressão automática para modelagem com modelo de crescimento exponencial
 v<-autofitVariogram(A~1,Mesure,model = "Exp")
-Krigagem simples com R
+##Krigagem simples com R
 prediction <-krige(formula=A~1, Mesure, Grille, model="Exp")
-Saída de dados para o QGIS
+##Saída de dados para o QGIS
 result<-raster(prediction)
 proj4string(Layer)->crs
 proj4string(result)<-crs
