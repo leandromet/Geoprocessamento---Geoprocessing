86f2757c10c916cfc9d1bd68e03711b3d9d14a2d
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-09-02 12:02:24 -0300

    Update postgis_topology_testing_topology.sql

--- postgis_topology_testing_topology.sql
+++ postgis_topology_testing_topology.sql
@@ -13,19 +13,79 @@ SELECT count(distinct idt_imovel) FROM sfb_dados.geom_mt;
 SELECT * FROM sfb_topo.mt_topo3 limit 1;
 --1980;1417948;"(4,1,1981,3)"
 
+-- select all that is in one registry from the source geometry table
 SELECT id, geom, idt_imovel FROM sfb_dados.geom_mt limit 1;
-100899;"01060000204212000001000000010300000001000000DA0000008533C528FC054CC0DB67E3E4016F2AC06EAA64B12D064CC0AC90C327936D2AC0E4DAFA63F20A4CC075B45E4DF3762AC0AA8744A7810B4CC04D0326EB0C782AC0A606F86D7E0B4CC04AFC66391E782AC02191EF307D0B4CC01A08D06524782AC0039BFE227C0B (...)";2762049
+--100899;"01060000204212000001000000010300000001000000DA0000008533C528FC054CC0DB67E3E4016F2AC06EAA64B12D064CC0AC90C327936D2AC0E4DAFA63F20A4CC075B45E4DF3762AC0AA8744A7810B4CC04D0326EB0C782AC0A606F86D7E0B4CC04AFC66391E782AC02191EF307D0B4CC01A08D06524782AC0039BFE227C0B (...)";2762049
 
+-- calculate area on topogeometry
 SELECT ST_Area(topo) FROM sfb_topo.mt_topo3 where idt_imovel = 1417948;
-0.00029206950898525
+--0.00029206950898525
 
+-- calculate area on geometry, same registry
 SELECT ST_Area(geom) FROM sfb_dados.geom_mt where idt_imovel = 1417948;
-0.000292069510311885
+--0.000292069510311885
 
---original
+--original table calculate the area of all geometries
 SELECT sum(ST_Area(geom)) FROM sfb_dados.geom_mt;
-59.9794519423682
-183ms
+--Result 59.9794519423682
+--time 183ms
+
+
+--topology table calculate the area of geometries that were correctly inserted on topology table
+SELECT sum(ST_Area(topo)) FROM sfb_topo.mt_topo3;
+-- Result 55.7808649152464
+-- time 5:17 min
+
+--original table calculate the area of geometries that were correctly inserted on topology table
+SELECT sum(ST_Area(geom)) FROM sfb_dados.geom_mt, sfb_topo.mt_topo3 where geom_mt.idt_imovel = mt_topo3.idt_imovel;
+--55.7808275474501
+--time < 200ms
+
+
+--original table calculate the area of geometries NOT inserted on topology table
+SELECT sum(ST_Area(geom)) FROM sfb_dados.geom_mt where geom_mt.idt_imovel not in ( select geom_mt.idt_imovel from sfb_dados.geom_mt, sfb_topo.mt_topo3 where geom_mt.idt_imovel = mt_topo3.idt_imovel);
+--4.19862439491804
+
+-- Select the geometries that were not inserted
+create table sfb_result.geom_mt_not_topo as SELECT id,idt_imovel,geom  FROM sfb_dados.geom_mt where geom_mt.idt_imovel not in ( select geom_mt.idt_imovel from sfb_dados.geom_mt, sfb_topo.mt_topo3 where geom_mt.idt_imovel = mt_topo3.idt_imovel);
+
+-- Find Intersections, both did not finish
+SELECT count(ST_Intersects(mt1.geom, mt2.geom)) FROM sfb_dados.geom_mt mt1, sfb_dados.geom_mt mt2 where mt1.id < mt2.id;
+
+
+SELECT count(ST_Intersects(mt1.topo, mt2.topo)) FROM sfb_topo.mt_topo3 mt1, sfb_topo.mt_topo3 mt2 where mt1.gid < mt2.gid;
+
+--Self intersecting multiple count
+
+SELECT count(ST_Intersects(mt1.geom, mt2.geom)) FROM sfb_dados.geom_mt mt1, sfb_dados.geom_mt mt2 , sfb_topo.mt_topo3 where mt1.id < mt2.id and mt2.id<5000 and mt1.idt_imovel = mt_topo3.idt_imovel;
+--12490340
+--10sec
+
+SELECT count(ST_Intersects(mt1.topo, mt2.topo)) FROM sfb_topo.mt_topo3 mt1, sfb_topo.mt_topo3 mt2 where mt1.gid < mt2.gid and mt2.gid<5000 and geom_mt.idt_imovel = mt_topo3.idt_imovel;
+--498501
+--43min
+
+
+--touches itself
+SELECT count(ST_Touches(mt1.geom, mt2.geom)) FROM sfb_dados.geom_mt mt1, sfb_dados.geom_mt mt2 , sfb_topo.mt_topo3 where mt1.id < mt2.id and mt2.id<5000 and mt1.idt_imovel = mt_topo3.idt_imovel;
+--12490340
+
+-- Intersects corrected
+
+SELECT count(CASE WHEN ST_Intersects(mt1.geom, mt2.geom) THEN 1 END) FROM sfb_dados.geom_mt mt1, sfb_dados.geom_mt mt2 where mt1.id < mt2.id and mt2.id<2000;
+--60
+--3.8sec
+
+SELECT count(CASE WHEN ST_Intersects(mt1.topo, mt2.topo) THEN 1 END) FROM sfb_topo.mt_topo3 mt1, sfb_topo.mt_topo3 mt2, sfb_dados.geom_mt where mt1.gid < mt2.gid and mt2.gid<2000 and geom_mt.idt_imovel = mt1.idt_imovel and mt2.id<2000;
+
+
+-- Intersects topology corrected
+
+SELECT count(CASE WHEN ST_Intersects(mt1.topo, mt2.topo) THEN 1 END) FROM sfb_topo.mt_topo3 mt1, sfb_topo.mt_topo3 mt2 where mt1.gid < mt2.gid and mt2.gid<2000;
+7
+
+
+-- Some data on the created topogeoemtry table
 
 total nodes
 426146
@@ -89,57 +149,3 @@ total edges
 left_edge=right_edge
 10883
 
-
-
-
---apenas os inseridos na topologia
-SELECT sum(ST_Area(topo)) FROM sfb_topo.mt_topo3;
-55.7808649152464
-5:17 minutos
-
-SELECT sum(ST_Area(geom)) FROM sfb_dados.geom_mt, sfb_topo.mt_topo3 where geom_mt.idt_imovel = mt_topo3.idt_imovel;
-55.7808275474501
-
-
-
-SELECT sum(ST_Area(geom)) FROM sfb_dados.geom_mt where geom_mt.idt_imovel not in ( select geom_mt.idt_imovel from sfb_dados.geom_mt, sfb_topo.mt_topo3 where geom_mt.idt_imovel = mt_topo3.idt_imovel);
-4.19862439491804
-
-create table sfb_result.geom_mt_not_topo as SELECT id,idt_imovel,geom  FROM sfb_dados.geom_mt where geom_mt.idt_imovel not in ( select geom_mt.idt_imovel from sfb_dados.geom_mt, sfb_topo.mt_topo3 where geom_mt.idt_imovel = mt_topo3.idt_imovel);
-
-SELECT count(ST_Intersects(mt1.geom, mt2.geom)) FROM sfb_dados.geom_mt mt1, sfb_dados.geom_mt mt2 where mt1.id < mt2.id;
-
-
-SELECT count(ST_Intersects(mt1.topo, mt2.topo)) FROM sfb_topo.mt_topo3 mt1, sfb_topo.mt_topo3 mt2 where mt1.gid < mt2.gid;
-
---intersecta ele próprio
-
-SELECT count(ST_Intersects(mt1.geom, mt2.geom)) FROM sfb_dados.geom_mt mt1, sfb_dados.geom_mt mt2 , sfb_topo.mt_topo3 where mt1.id < mt2.id and mt2.id<5000 and mt1.idt_imovel = mt_topo3.idt_imovel;
-12490340
-10segundos
-
-SELECT count(ST_Intersects(mt1.topo, mt2.topo)) FROM sfb_topo.mt_topo3 mt1, sfb_topo.mt_topo3 mt2 where mt1.gid < mt2.gid and mt2.gid<5000 and geom_mt.idt_imovel = mt_topo3.idt_imovel;
-498501
-43minutos
-
-
---Toca ele próprio
-SELECT count(ST_Touches(mt1.geom, mt2.geom)) FROM sfb_dados.geom_mt mt1, sfb_dados.geom_mt mt2 , sfb_topo.mt_topo3 where mt1.id < mt2.id and mt2.id<5000 and mt1.idt_imovel = mt_topo3.idt_imovel;
-12490340
-
--- Intersects corrigido
-
-SELECT count(CASE WHEN ST_Intersects(mt1.geom, mt2.geom) THEN 1 END) FROM sfb_dados.geom_mt mt1, sfb_dados.geom_mt mt2 where mt1.id < mt2.id and mt2.id<2000;
-60
-3.8sec
-
-SELECT count(CASE WHEN ST_Intersects(mt1.topo, mt2.topo) THEN 1 END) FROM sfb_topo.mt_topo3 mt1, sfb_topo.mt_topo3 mt2, sfb_dados.geom_mt where mt1.gid < mt2.gid and mt2.gid<2000 and geom_mt.idt_imovel = mt1.idt_imovel and mt2.id<2000;
-
-
-
-
-
--- Intersects topo
-
-SELECT count(CASE WHEN ST_Intersects(mt1.topo, mt2.topo) THEN 1 END) FROM sfb_topo.mt_topo3 mt1, sfb_topo.mt_topo3 mt2 where mt1.gid < mt2.gid and mt2.gid<2000;
-7
