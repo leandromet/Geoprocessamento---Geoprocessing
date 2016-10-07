652aa3efb8b7c49ddb8ae998ab1d64200555bc30
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2016-10-06 22:55:46 -0300

    Update kriging_qgis_r.r

--- kriging_qgis_r.r
+++ kriging_qgis_r.r
@@ -1,10 +1,9 @@
-
 ##Basic statistics=group
 ##Layer=vector
 ##Field=Field Layer
+##Selection=Selection Exp;Log;Sph;Gau;Exc;Mat;Cir;Lin;Bes;Pen;Per;Wav;Hol;Leg;Ste
 ## by= number 0.1
 ##Output=output raster
-##Bibliotecas utilizadas para a krigagem
 library(gstat)
 library(rgl)
 library("spatstat")
@@ -13,18 +12,21 @@ install.packages("pls")
 library (pls)
 library(automap)
 library(raster)
-##Abertura de dados e alocação de variáveis
 Y<-as.factor(Layer[[Field]])
 attribut<-as.data.frame(Y)
 A<-as.numeric(Y)
 for(j in (1:length(levels(Y))))
 for(i in 1:dim(attribut)[1]){
 if (attribut[i,1]==levels(Y)[j]){
-A[i]=j } }
-##Carregamento das informações espaciais
+A[i]=j
+}
+}
 coords<-coordinates(Layer)
 Mesure<- data.frame(LON=coords[,1], LAT=coords[,2], A)
 coordinates(Mesure)<-c("LON","LAT")
+Models<-c("Exp","Log","Sph","Gau","Exc","Mat","Cir","Lin","Bes","Pen","Per","Wav","Hol","Leg","Ste")
+Selection<-Selection+1
+select_model<-Models[Selection]
 MinX<-min(coords[,1])
 MinY<-min(coords[,2])
 MaxX<-max(coords[,1])
@@ -34,17 +36,12 @@ Seqy<-seq(MinY, MaxY, by=by)
 MSeqx<-rep(Seqx, length(Seqy))
 MSeqy<-rep(Seqy, length(Seqx))
 MSeqy <- sort(MSeqy, decreasing=F)
-##Criação da planilha para execução no R
 Grille <- data.frame(X=MSeqx, Y=MSeqy)
 coordinates(Grille)=c("X","Y")
 gridded(Grille)<-TRUE
-##Extração do variograma com regressão automática para modelagem com modelo de crescimento exponencial
-v<-autofitVariogram(A~1,Mesure,model = "Exp")
-##Krigagem simples com R
-prediction <-krige(formula=A~1, Mesure, Grille, model="Exp")
-##Saída de dados para o QGIS
+v<-autofitVariogram(A~1,Mesure,model = select_model)
+prediction <-krige(formula=A~1, Mesure, Grille, model=v$var_model)
 result<-raster(prediction)
 proj4string(Layer)->crs
 proj4string(result)<-crs
 Output<-result
-
