1d178f895f48966f072bf49c3e8689bb6672210c
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2019-06-11 11:17:19 -0300

    Update car_dinamica_contar_pontos_vertices.sql

--- car_dinamica_contar_pontos_vertices.sql
+++ car_dinamica_contar_pontos_vertices.sql
@@ -13,4 +13,11 @@ FROM
 
 
 
-
+SELECT 
+  imovel.ind_status_imovel, 
+  count(imovel.idt_imovel)
+FROM 
+  usr_geocar_aplicacao.imovel
+  where  dat_atualizacao >= to_date('31/12/2018','dd/mm/yyyy') 
+  group by ind_status_imovel
+  order by ind_status_imovel;
