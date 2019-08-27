020ded6728c7dd87f34014b4c60b01b8a2e04d3c
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2019-08-27 15:36:06 -0300

    Update car_que_2019.sql

--- car_que_2019.sql
+++ car_que_2019.sql
@@ -18,10 +18,6 @@ create table proc_sfb.focos_2019_amz_car_unicos_dat as
 select distinct on (focos_2019_amz_car.id)  focos_2019_0722a0823.id, datahora, focos_2019_0722a0823.geom,idt_imovel from proc_sfb.focos_2019_amz_car , proc_sfb.focos_2019_0722a0823
 where focos_2019_amz_car.id = focos_2019_0722a0823.id
 
-create table proc_sfb.focos_class_antrop as select distinct(focos_2019_0722a0823.id), gid, bioma,
-datahora,  focos_2019_0722a0823.geom from
-base_referencia.area_antropizada_apos_2008 , proc_sfb.focos_2019_0722a0823
-where  st_intersects(area_antropizada_apos_2008.the_geom, focos_2019_0722a0823.geom)
 
 
 
@@ -39,6 +35,9 @@ geoserver.tema_simp_26_imovel, proc_sfb.focos_2019_0722a0823
 where st_intersects (tema_simp_26_imovel.geom, focos_2019_0722a0823.geom)
 
 
+
+
+
 SELECT "focos_2019_car"."bioma",Count(*) 
 FROM "focos_2019_car"
 GROUP BY "focos_2019_car"."bioma"
@@ -46,3 +45,46 @@ GROUP BY "focos_2019_car"."bioma"
 
 
 
+create table proc_sfb.focos_class_consol as select distinct(focos_2019_0722a0823.id), gid, bioma,
+datahora,  focos_2019_0722a0823.geom from
+base_referencia.area_consolidada , proc_sfb.focos_2019_0722a0823
+where  st_intersects(area_consolidada.the_geom, focos_2019_0722a0823.geom)
+
+
+
+create table proc_sfb.focos_class_antrop as select distinct(focos_2019_0722a0823.id), gid, bioma,
+datahora,  focos_2019_0722a0823.geom from
+base_referencia.area_antropizada_apos_2008 , proc_sfb.focos_2019_0722a0823
+where  st_intersects(area_antropizada_apos_2008.the_geom, focos_2019_0722a0823.geom)
+
+
+
+
+create table proc_sfb.focos_class_regen_car as select distinct(id), gid, bioma,idt_imovel,
+datahora,  focos_class_regen.geom from
+proc_sfb.focos_class_regen , geoserver.tema_simp_26_imovel
+where  st_intersects(focos_class_regen.geom, tema_simp_26_imovel.geom)
+
+
+
+
+create table proc_sfb.focos_class_rvn_car as select distinct(id), gid, bioma,idt_imovel,
+datahora,  focos_class_rvn.geom from
+proc_sfb.focos_class_rvn , geoserver.tema_simp_26_imovel
+where  st_intersects(focos_class_rvn.geom, tema_simp_26_imovel.geom)
+
+
+
+
+create table proc_sfb.focos_class_antrop_car as select distinct(id), gid, bioma,idt_imovel,
+datahora,  focos_class_antrop.geom from
+proc_sfb.focos_class_antrop , geoserver.tema_simp_26_imovel
+where  st_intersects(focos_class_antrop.geom, tema_simp_26_imovel.geom)
+
+
+
+
+create table proc_sfb.focos_class_consol_car as select distinct(id), gid, bioma,idt_imovel,
+datahora,  focos_class_consol.geom from
+proc_sfb.focos_class_consol , geoserver.tema_simp_26_imovel
+where  st_intersects(focos_class_consol.geom, tema_simp_26_imovel.geom)
