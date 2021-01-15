a8ccf09e1e685a26b21fc57ce60622693277325d
Leandro M Biondo <leandro.biondo@florestal.gov.br>
2021-01-15 10:53:36 -0300

    Update inma_materialized_view_rima_fauna.sql

--- inma_materialized_view_rima_fauna.sql
+++ inma_materialized_view_rima_fauna.sql
@@ -1,44 +1,56 @@
--- View: rima_geoserver.pontos_flora_niveis
+-- View: rima_geoserver.postos_fauna_niveis
 
--- DROP MATERIALIZED VIEW rima_geoserver.pontos_flora_niveis;
+-- DROP MATERIALIZED VIEW rima_geoserver.postos_fauna_niveis;
 
-CREATE MATERIALIZED VIEW rima_geoserver.pontos_flora_niveis
+CREATE MATERIALIZED VIEW rima_geoserver.postos_fauna_niveis
 TABLESPACE pg_default
 AS
- SELECT DISTINCT table4.phylum,
-    table4.class,
-    table4.ordem,
-    table4.family,
-    table4.genus,
-    table4.num_gen
-   FROM ( SELECT DISTINCT tale3.class,
-            tale3.ordem,
-            tale3.family,
-            tale3.phylum,
-            tale3.genus,
-            tale3.num_gen
-           FROM ( SELECT DISTINCT table2.ordem,
-                    table2.phylum,
-                    table2.family,
-                    table2.class,
-                    table2.genus,
-                    table2.num_gen
-                   FROM ( SELECT DISTINCT tabela.family,
-                            tabela.phylum,
-                            tabela.ordem,
-                            tabela.class,
-                            tabela.genus,
-                            tabela.num_gen
-                           FROM ( SELECT DISTINCT pontos_flora.genus,
-                                    pontos_flora.phylum,
-                                    pontos_flora.ordem,
-                                    pontos_flora.class,
-                                    pontos_flora.family,
-                                    count(*) AS num_gen
-                                   FROM rima_geoserver.pontos_flora
-                                  GROUP BY pontos_flora.genus, pontos_flora.phylum, pontos_flora.ordem, pontos_flora.class, pontos_flora.family) tabela) table2) tale3) table4
-  ORDER BY table4.phylum, table4.class, table4.ordem, table4.family
+ SELECT DISTINCT table5.phylum,
+    table5.class,
+    table5.ordem,
+    table5.family,
+    table5.genus,
+    table5.species,
+    table5.num_gen
+   FROM ( SELECT DISTINCT table4.class,
+            table4.ordem,
+            table4.family,
+            table4.phylum,
+            table4.genus,
+            table4.species,
+            table4.num_gen
+           FROM ( SELECT DISTINCT tale3.ordem,
+                    tale3.phylum,
+                    tale3.family,
+                    tale3.class,
+                    tale3.genus,
+                    tale3.species,
+                    tale3.num_gen
+                   FROM ( SELECT DISTINCT table2.family,
+                            table2.phylum,
+                            table2.ordem,
+                            table2.class,
+                            table2.genus,
+                            table2.species,
+                            table2.num_gen
+                           FROM ( SELECT DISTINCT tabela.genus,
+                                    tabela.phylum,
+                                    tabela.family,
+                                    tabela.ordem,
+                                    tabela.class,
+                                    tabela.species,
+                                    tabela.num_gen
+                                   FROM ( SELECT DISTINCT pontos_fauna.species,
+    pontos_fauna.genus,
+    pontos_fauna.phylum,
+    pontos_fauna.ordem,
+    pontos_fauna.class,
+    pontos_fauna.family,
+    count(*) AS num_gen
+   FROM rima_geoserver.pontos_fauna
+  GROUP BY pontos_fauna.genus, pontos_fauna.phylum, pontos_fauna.ordem, pontos_fauna.class, pontos_fauna.family, pontos_fauna.species) tabela) table2) tale3) table4) table5
+  ORDER BY table5.phylum, table5.class, table5.ordem, table5.family
 WITH DATA;
 
-ALTER TABLE rima_geoserver.pontos_flora_niveis
+ALTER TABLE rima_geoserver.postos_fauna_niveis
     OWNER TO postgres;
